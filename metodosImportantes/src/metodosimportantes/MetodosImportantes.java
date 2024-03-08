42c60bef09bfde557c4e1109960ba00adbc20165
unknown <sheylla10>
2024-03-08 08:20:46 -0600

    Uploading methods

--- metodosImportantes/src/metodosimportantes/MetodosImportantes.java
+++ metodosImportantes/src/metodosimportantes/MetodosImportantes.java
@@ -14,10 +14,9 @@ public class MetodosImportantes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //
+        //
     }
-    
-    
+
     //método de listar en el arbol - posiblemente usar jlist
     /*public void listar_todo(File p_raiz, DefaultMutableTreeNode nodo){  
     try{
@@ -51,8 +50,6 @@ public class MetodosImportantes {
         e.printStackTrace();
     }
     }*///end of recursive method
-    
-    
     //crear nueva carpeta
     /*
     public File createNewFolder(File containingDir) {
@@ -62,4 +59,107 @@ public class MetodosImportantes {
     }*///end of create new folder
     
     
+    
+    //métodos para crud de arboles
+    /*
+    public void load(String nombre) throws FileNotFoundException, IOException {
+        DefaultTableModel modelo = (DefaultTableModel) jTable.getModel();
+        modelo.setRowCount(0);
+
+        //le mando el nombre para que me lo busque el que se llama asi
+        FileReader fr = new FileReader(nombre);
+        BufferedReader br = new BufferedReader(fr);
+        String miline;
+
+        //lo añado a la row
+        while ((miline = br.readLine()) != null) {
+            String[] info = miline.split(",");
+            modelo.addRow(info);
+        }
+
+        //cierro
+        br.close();
+        fr.close();
+    }
+
+    public void crear(String nombre) throws IOException {
+        //crear el archivo con el nombre
+        File archivo = new File(nombre);
+        FileWriter fw = new FileWriter(archivo);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        //agarro las filas y columnas de la tabla
+        int fila = jTable.getRowCount();
+        int columna = jTable.getColumnCount();
+        String[][] info = new String[fila][columna];
+
+        //escribo la info
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                info[i][j] = (String) jTable.getValueAt(i, j);
+                bw.write(info[i][j] + ";");
+            }
+            bw.newLine();
+        }
+        
+        bw.close();
+        fw.close();
+
+    }
+
+    public void clear() {
+        DefaultTableModel modelo2 = (DefaultTableModel) jTable.getModel();
+        for (int i = 0; i < modelo2.getRowCount(); i++) {
+            for (int j = 0; j < modelo2.getColumnCount(); j++) {
+                modelo2.setValueAt("", i, j);
+            }
+        }
+    }
+
+    public void refresh() {
+        DefaultTreeModel m = (DefaultTreeModel) jTree.getModel();
+        DefaultMutableTreeNode c = new DefaultMutableTreeNode("Archivo");
+        m.setRoot(c);
+        File x = new File("./");
+        File[] f = x.listFiles();
+        for (File miarchivo : f) {
+            if (miarchivo.getName().endsWith(".txt")) {
+                c.add(new DefaultMutableTreeNode(miarchivo.getName()));
+            }
+        }
+        jTree.setModel(m);
+        m.reload();
+    }*///termina metodos crud
+    
+    
+    //método combo box
+    /*
+    private void tabStateChanged(javax.swing.event.ChangeEvent evt) {                                 
+        // TODO add your handling code here:
+        if (tab.getSelectedIndex() == 1) {
+            adminPersona ap=new adminPersona("./aleros.cbm");
+            ap.cargarArchivo();
+            DefaultComboBoxModel modelo
+                        = new DefaultComboBoxModel( 
+                                ap.getListaPersonas().toArray() );
+            cb_amigos.setModel(modelo);
+            
+           
+        }
+    } *///end
+    
+       //resize image
+    /*        JFileChooser fc = new JFileChooser();
+        FileFilter filtro = new FileNameExtensionFilter("Imagenes", 
+                "png", "jpg", "jpeg", "gif");
+        fc.setFileFilter(filtro);
+        File archivo;
+        int op = fc.showOpenDialog(this);
+        if (op == JFileChooser.APPROVE_OPTION) {
+            archivo = fc.getSelectedFile();
+            Image img = Toolkit.getDefaultToolkit().createImage(
+                    archivo.getPath()).getScaledInstance(100, 100, 0);
+            this.lb_foto.setIcon(new ImageIcon(img));
+
+        }*///end
 }
